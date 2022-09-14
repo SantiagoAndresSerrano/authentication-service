60353e5540865d21b9fa24737b80226225eaf660
Santiago Serrano <62350078+SantiagoAndresSerrano@users.noreply.github.com>
2022-09-13 20:36:24 -0500

    First structure

---
+++ src/test/java/ufps/edu/co/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package ufps.edu.co.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
